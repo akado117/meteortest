e3b26fd477b498ed96ec591c09042743b896fbb9
Alex <akaidan@mtu.edu>
2015-07-22 23:55:40 -0400

    Added sample project
    For jumping between machines

---
+++ meteorSample/simple-todos/simple-todos.js
@@ -0,0 +1,36 @@
+if (Meteor.isClient) {
+  // counter starts at 0
+  //Session.setDefault('counter', 0);
+
+/*
+  Template.hello.helpers({
+    counter: function () {
+      return Session.get('counter');
+    }
+  });
+
+  Template.hello.events({
+    'click button': function () {
+      // increment the counter when button is clicked
+      Session.set('counter', Session.get('counter') + 1);
+    }
+  });
+}
+*/
+
+Template.body.helpers({
+  tasks: [
+  {text: "task1"},
+  {text: "task2"},
+  {text: "task3"}
+  ]
+});
+
+
+/*
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+*/
+}
